23e302b97200a99665ecce0e7f167400cc470994
Van Hohenheim <0xhohenheim@gmail.com>
2024-10-01 06:54:33 +0530

    feat: base form

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as logo } from "./logo.png";
 export { default as background } from "./background.png";
 export { default as coinbaseBadge } from "./coinbaseBadge.png";
+export { default as ethereumLogo } from "./ethereumLogo.png";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import styles from "./Home.module.scss";
 import { useGlobal } from "../../contexts/global.context";
+import { useAccount } from "wagmi";
+import { ethereumLogo } from "../../assets";
+import { useAppKit } from "@reown/appkit/react";
 
 export default function Home() {
     const { selectedMode } = useGlobal();
+    const { isConnected } = useAccount();
+    const { open } = useAppKit();
     const [selectedTab, setSelectedTab] = useState("stake");
 
     useEffect(() => {
-        setSelectedTab("stake")
+        setSelectedTab("stake");
     }, [selectedMode]);
 
     return (
@@ -36,7 +41,43 @@ export default function Home() {
                     }
                 </div>
                 <div className={styles.contentContainer}>
-
+                    {/* AVAILABLE BALANCES */}
+                    {isConnected &&
+                    <div className={styles.availableContainer}>
+                        <span className={styles.key}>Available to {selectedTab}</span>
+                        <span className={styles.value}>0.1234 ETH</span>
+                    </div>
+                    }
+                    {/* INPUT CONTAINER */}
+                    <div className={styles.inputSection}>
+                        <span className={styles.title}>{selectedTab}</span>
+                        <div className={styles.inputContainer}>
+                            <img src={ethereumLogo} alt="Ethereum" />
+                            <span className={styles.title}>ETH amount</span>
+                            <input type="number" step={0.00001} />
+                        </div>
+                    </div>
+                    {!isConnected &&
+                    <div className={styles.connectWallet} onClick={() => open()}>
+                        Connect Wallet
+                    </div>
+                    }
+                    <div className={styles.infoContainer}>
+                        <div className={styles.row}>
+                            <span className={styles.title}>Staking Type</span>
+                            <span className={styles.subtitle}>Native</span>
+                        </div>
+                        <div className={styles.hr} />
+                        <div className={styles.row}>
+                            <span className={styles.title}>Provider</span>
+                            <span className={styles.subtitle}>Coinbase</span>
+                        </div>
+                        <div className={styles.hr} />
+                        <div className={styles.row}>
+                            <span className={styles.title}>Transaction Fee</span>
+                            <span className={styles.subtitle}>0.00 ETH/$0.00</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
